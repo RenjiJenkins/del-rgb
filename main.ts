fc6fe07f4a2a6a71c7e65906fa358681aa9a573c
RenjiJenkins <82725012+MGFsLive@users.noreply.github.com>
2021-11-24 11:41:47 -0500

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,40 @@
+let intensité = 0
+function bleu () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+}
+function blanc () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+}
+function mauve () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+}
+function rouge () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+}
+function vert () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+}
+basic.forever(function () {
+    intensité = pins.analogReadPin(AnalogPin.P0)
+    if (intensité >= 900) {
+        blanc()
+    } else if (intensité < 400 && intensité > 200) {
+        bleu()
+    } else if (intensité < 600 && intensité > 400) {
+        rouge()
+    } else if (intensité < 800 && intensité > 600) {
+        mauve()
+    } else {
+        vert()
+    }
+})
